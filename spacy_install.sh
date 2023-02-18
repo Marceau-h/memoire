55383419c86ba4f45180d0e597c8cefc44273618
marceau <mail@placeholder.com>
2023-02-18 17:00:02 +0100

    Stats et courbes
    Sur :
    - le nombre d'hapaxes
    - le taux de lexicalité
    - le ratio voc/token
    
    TODO : il y a t-il un lien entre certaines méta-données et l'évolution de ces valeurs ?

---
+++ spacy_install.sh
@@ -0,0 +1,32 @@
+python3.11 -m pip install -U pip setuptools wheel
+python3.11 -m pip install -U 'spacy[transformers,lookups]'
+python3.11 -m spacy download en_core_web_sm
+python3.11 -m spacy download en_core_web_md
+python3.11 -m spacy download en_core_web_lg
+python3.11 -m spacy download en_core_web_trtf
+
+python3.11 -m spacy download fr_core_news_sm
+python3.11 -m spacy download fr_core_news_md
+python3.11 -m spacy download fr_core_news_lg
+python3.11 -m spacy download fr_core_news_lg
+
+python3.11 -m spacy download ca_core_news_sm
+python3.11 -m spacy download zh_core_web_sm
+python3.11 -m spacy download hr_core_news_sm
+python3.11 -m spacy download da_core_news_sm
+python3.11 -m spacy download nl_core_news_sm
+python3.11 -m spacy download fi_core_news_sm
+python3.11 -m spacy download de_core_news_sm
+python3.11 -m spacy download it_core_news_sm
+python3.11 -m spacy download ja_core_news_sm
+python3.11 -m spacy download ko_core_news_sm
+python3.11 -m spacy download lt_core_news_sm
+python3.11 -m spacy download mk_core_news_sm
+python3.11 -m spacy download nb_core_news_sm
+python3.11 -m spacy download pl_core_news_sm
+python3.11 -m spacy download pt_core_news_sm
+python3.11 -m spacy download ro_core_news_sm
+python3.11 -m spacy download ru_core_news_sm
+python3.11 -m spacy download es_core_news_sm
+python3.11 -m spacy download sv_core_news_sm
+python3.11 -m spacy download uk_core_news_sm
