31a749eb46210217c3b01d9583f3180b090a4c17
marceau <mail@placeholder.com>
2023-02-27 16:13:39 +0100

    venv activate

--- firstlaunch.sh
+++ firstlaunch.sh
@@ -1,4 +1,5 @@
-python3 -m venv venv & git clone https://github.com/Antonomaz/Corpus.git 2> /dev/null || cd Corpus && git pull && cd ..
+python3 -m venv venv || source ./venv/bin/activate
+git clone https://github.com/Antonomaz/Corpus.git 2> /dev/null || cd Corpus && git pull && cd ..
 
 ./venv/bin/python -m pip install -U pip
 ./venv/bin/python -m pip install -r requirements.txt
